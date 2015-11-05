8213d254711f16562c9cfa416e32ac535da1247b
barbuandrei <barbuandrei@34f8ff9c-259e-4c38-875d-bd3976659ca2>
2015-11-05 18:55:29 +0000

    project commit

---
+++ E-learning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_learning.Startup))]
+namespace E_learning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
