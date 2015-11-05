49136ef885ad551f350915f071c0d9445685bfe2
curchibianca <curchibianca@34f8ff9c-259e-4c38-875d-bd3976659ca2>
2015-11-05 19:28:07 +0000

    Folder: Infrastructure Adding: GenericRepository && UnitOfWork

---
+++ E-learning/Infrastructure/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_learning.Infrastructure
+{
+    public class GenericRepository<T> where T : class
+    {
+
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T InsertEntity(T entity)
+        {
+            return _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            var entry = _context.Entry(entity);
+            entry.State = EntityState.Deleted;
+        }
+
+        public void Update(T entity)
+        {
+            var entry = _context.Entry(entity);
+            _dbSet.Attach(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public T GetByID(int ID)
+        {
+            return _dbSet.Find(ID);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IEnumerable<T> Find(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
